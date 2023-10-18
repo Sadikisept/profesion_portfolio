400a80fd7f0905ff3240afd0683e08eb0a8ca8f1
unknown <sadikisept26@gmail.com>
2023-10-18 18:14:21 +0200

    add js code

--- js/main.js
+++ js/main.js
@@ -80,7 +80,7 @@ contactForm.reset()
     const sections = document.querySelectorAll('section[id]')
 
     const scrollActive = () =>{
-        const scrollY = window.pageYoffset
+        const scrolly = window.pageYOffset
 
         sections.forEach(current =>{
             const sectionHeight = current.offsetHeight,
@@ -89,12 +89,25 @@ contactForm.reset()
             sectionsClass = document.querySelector('.nav__menu a [href*=' + sectionId + ']')
        
             if(scrolly > sectionTop && scrolly <= sectionHeight){
-                sectionClass.add('active-link')
+                sectionsClass.add('active-link')
             }else{
-                sectionClass.classList.remove('active-link')
+                sectionsClass.classList.remove('active-link')
             }
         })
     }
     window.addEventListener('scroll', scrollActive)
 
-    
+   /*=============SCROLL REVEAR ANIMATION===================*/ 
+   const sr = scrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    //reset: true // Animation repeat
+   })
+
+   sr.reveal(`.home__data, .home__social, .contact__container, .footer__container`)
+   sr.reveal(`.home__image`, {origin: 'bottom'})
+   sr.reveal(`.about__data, .skills__data`,{origin: 'left'})
+   sr.reveal(`.about__image, .skills__content`,{origin: 'right'})
+   sr.reveal(`.services__card, .projects__card`,{interval:100})
